8c16e767417ca938bfb5d0508a110a88e02ec910
Fabian Siegel <fabians1999@gmail.com>
2020-06-09 10:42:47 +0200

    Moved atlassian jwt stuff into its own package

--- middleware/authentication.go
+++ middleware/authentication.go
@@ -1,14 +1,9 @@
 package middleware
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
-	"sort"
 	"strings"
 
 	gonnect "github.com/craftamap/atlas-gonnect"
@@ -35,93 +30,6 @@ func extractUnverifiedClaims(tokenStr string, validator jwt.Keyfunc) (jwt.MapCla
 		log.Printf("Invalid JWT Token")
 		return nil, false
 	}
-}
-
-func CreateQueryStringHash(req *http.Request, checkBodyForParam bool, baseUrlString string) string {
-	const CANONICAL_QUERY_SEPARATOR = "&"
-	canonicalizeUri := func() string {
-		path := req.URL.Path
-		//TODO: Handle error here
-		baseUrl, _ := url.Parse(baseUrlString)
-		baseUrlPath := baseUrl.Path
-
-		// If path and baseUrlPath have the same path, trim the path
-		if strings.HasPrefix(path, baseUrlPath) {
-			path = strings.TrimPrefix(path, baseUrlPath)
-		}
-
-		// if the path is an empty string now, this means the baseUrlPath AND the path were "/".
-		// We therefore return "/"
-		// Otherwise, the path is currently the path after the baseUrlPath
-		if len(path) == 0 {
-			return "/"
-		}
-
-		// If the separator is not URL encoded then the following URLs have the same query-string-hash:
-		//   https://djtest9.jira-dev.com/rest/api/2/project&a=b?x=y
-		//   https://djtest9.jira-dev.com/rest/api/2/project?a=b&x=y
-		strings.ReplaceAll(path, CANONICAL_QUERY_SEPARATOR, url.QueryEscape(CANONICAL_QUERY_SEPARATOR))
-
-		if !strings.HasPrefix(path, "/") {
-			path = "/" + path
-		}
-
-		if len(path) > 1 && strings.HasSuffix(path, "/") {
-			path = strings.TrimSuffix(path, "/")
-		}
-
-		return path
-	}
-
-	canonicalizeQueryString := func() string {
-		queryParams := req.URL.Query()
-
-		if checkBodyForParam && len(queryParams) == 0 && (strings.ToUpper(req.Method) == "POST" || strings.ToUpper(req.Method) == "PUT") {
-			bodyReader, err := req.GetBody()
-			if err == nil {
-				bodyContent, err := ioutil.ReadAll(bodyReader)
-				if err == nil {
-					queryParams, _ = url.ParseQuery(string(bodyContent))
-				}
-			}
-		}
-
-		sortedQueryStrings := make([]string, 0)
-		query := make([]string, 0)
-		for key := range queryParams {
-			if key != "jwt" {
-				query = append(query, key)
-			}
-		}
-		sort.Strings(query)
-		for _, key := range query {
-			if key == "__proto__" {
-				continue
-			}
-
-			param := queryParams[key]
-			sort.Strings(param)
-			for idx, value := range param {
-				param[idx] = url.QueryEscape(value)
-			}
-			paramValue := strings.Join(param, ",")
-			sortedQueryStrings = append(sortedQueryStrings, url.QueryEscape(key)+"="+paramValue)
-		}
-		return strings.Join(sortedQueryStrings, "&")
-	}
-
-	createCanonicalRequest := func() string {
-		return strings.ToUpper(req.Method) +
-			CANONICAL_QUERY_SEPARATOR +
-			canonicalizeUri() +
-			CANONICAL_QUERY_SEPARATOR +
-			canonicalizeQueryString()
-	}
-
-	s := createCanonicalRequest()
-	h := sha256.New()
-	h.Write([]byte(s))
-	return hex.EncodeToString(h.Sum(nil))
 }
 
 func (h AuthenticationMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
